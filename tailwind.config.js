24874454c0eb445a23c3f6773d1fc53615184557
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-16 15:07:05 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyan: "#22d3ee"
+      }
+    }
+  },
+  plugins: []
+};
