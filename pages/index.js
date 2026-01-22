cbb535e3113bbf804e4983eb0d72cd699f33160d
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-22 13:55:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,3 @@
-const styles = {
-  page: {
-    minHeight: "100vh",
-    background: "#020617",
-    color: "#e5e7eb",
-    fontFamily: "system-ui",
-  },
-  title: {
-    fontSize: "32px",
-    fontWeight: "800",
-    textAlign: "center",
-    marginTop: "80px",
-  },
-};
-
 export default function Home() {
-  return (
-    <div style={styles.page}>
-      <h1 style={styles.title}>GrowFast</h1>
-    </div>
-  );
+  return <h1>GrowFast</h1>;
 }
