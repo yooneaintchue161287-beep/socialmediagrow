b0656a14cd67600b2279128a2a265b24e5d3345c
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-23 21:33:32 +0800

    Delete components/StatCard.js

--- components/StatCard.js
+++
@@ -1,8 +0,0 @@
-export default function StatCard({ title, value }) {
-  return (
-    <div className="bg-slate-800 rounded-xl p-4 min-w-[140px]">
-      <p className="text-sm text-gray-400">{title}</p>
-      <p className="text-xl font-bold text-cyan-400">{value}</p>
-    </div>
-  );
-}
